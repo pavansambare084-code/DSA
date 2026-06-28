af17dd6ab9010f9a0560fa3bef84bbd32ba7217a
Pavan Ashok Sambare Patil <pavansambare084@gmail.com>
2026-06-28 22:27:23 +0530

    Time: 0 ms (100%), Space: 8.9 MB (96.88%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.c
@@ -0,0 +1,11 @@
+int addDigits(int num) {
+    int sum=0;
+    if (num<10){
+        return num;
+    }
+    while(num!=0){
+        sum+=num%10;
+        num=num/10;
+    }
+    return addDigits(sum);
+}
