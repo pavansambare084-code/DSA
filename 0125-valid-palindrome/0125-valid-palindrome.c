036e415532d18df0d33ef4f9edbe200011395051
Pavan Ashok Sambare Patil <pavansambare084@gmail.com>
2026-07-08 13:57:20 +0530

    Time: 0 ms (100%), Space: 9.3 MB (58.98%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.c
+++ 0125-valid-palindrome/0125-valid-palindrome.c
@@ -1,21 +1,38 @@
 bool isPalindrome(char* s) {
-    int n = strlen(s);
-    char str[n];
-    int len=-1;
-    for(int i=0;i<n;i++){
-        if(isalnum(s[i])){
-            str[++len]=tolower(s[i]);
+    // int n = strlen(s);
+    // char str[n];
+    // int len=-1;
+    // for(int i=0;i<n;i++){
+    //     if(isalnum(s[i])){
+    //         str[++len]=tolower(s[i]);
+    //     }
+    // }
+    // int i=0;
+    // while(i<len){
+    //     if(str[i]==str[len]){
+    //     }
+    //     else{
+    //         return 0;
+    //     }
+    //     i++;
+    //     len--;
+    // }
+    int i=0,j=strlen(s)-1;
+    while(i<j){
+        while(i<j && !isalnum(s[i])){
+            i++;
         }
-    }
-    int i=0;
-    while(i<len){
-        if(str[i]==str[len]){
+        while(i<j && !isalnum(s[j])){
+            j--;
+        }
+        if(tolower(s[i])==tolower(s[j])){
+
         }
         else{
             return 0;
         }
         i++;
-        len--;
+        j--;
     }
     return 1;
 }
