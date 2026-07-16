b01a81c3c8edf42fff34b36ba03d4f4cadc46b5a
Pavan Ashok Sambare Patil <pavansambare084@gmail.com>
2026-07-16 22:35:27 +0530

    Time: 3 ms (65.77%), Space: 7.5 MB (98.9%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -2,6 +2,12 @@ class Solution {
 public:
     int fib(int n) {
         if(n<=1) return n;
-        return fib(n-1) + fib(n-2);
+        int a=0,b=1,sum=0;
+        for(int i=2;i<=n;i++){
+            sum=a+b;
+            a=b;
+            b=sum;
+        }
+        return sum;
     }
 };
