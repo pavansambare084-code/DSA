09fec800d5255a740945c3d2304646942373725a
Pavan Ashok Sambare Patil <pavansambare084@gmail.com>
2026-08-03 23:25:23 +0530

    Time: 3 ms (2.2%), Space: 8.9 MB (69.32%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n=s.size();
+        int cnt=0;
+        for(int i=n-1;i>=0;i--){
+            if(s[i]==' ' && cnt==0){
+                continue;
+            }
+            else{
+                if(s[i]==' ' || s[i]=='\n'){
+                    break;
+                }
+                else{
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+};
