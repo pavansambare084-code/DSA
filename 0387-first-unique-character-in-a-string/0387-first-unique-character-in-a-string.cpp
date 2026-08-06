abfc4096508a9733ad9687b5d45ecd9d0a3d84ef
Pavan Ashok Sambare Patil <pavansambare084@gmail.com>
2026-08-06 23:11:30 +0530

    Time: 4 ms (72.76%), Space: 14.7 MB (74.81%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int arr[26]={0},n=s.length();
+        for(int i=0;i<n;i++){
+            arr[s[i]-'a']++;
+        }
+        for(int i=0;i<n;i++){
+            if(arr[s[i]-'a']==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
