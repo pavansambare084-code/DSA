fddd032c3d1161c583364bedb3707660207d6ea0
Pavan Ashok Sambare Patil <pavansambare084@gmail.com>
2026-08-08 19:18:42 +0530

    Time: 11 ms (28.05%), Space: 128.3 MB (78.43%) - LeetHub

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int i=0,j=1;
+        vector<int> ans(nums.size());
+        for(int x:nums){
+            if(x>0){
+                ans[i]=x;
+                i+=2;
+            }
+            else{
+                ans[j]=x;
+                j+=2;
+            }
+        }
+            return ans;
+        }
+};
