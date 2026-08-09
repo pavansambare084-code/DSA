c9e1010a720d976d07576c6006cd93712cb6bb1b
Pavan Ashok Sambare Patil <pavansambare084@gmail.com>
2026-08-09 22:23:23 +0530

    Time: 3 ms (18.09%), Space: 8.6 MB (14.96%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if(x==0 || x==1) return x;
-        for(long long i=2;i<=x;i++){
-            long long pro=i*i,prev=(i-1)*(i-1);
-            if(pro==x){
-                return i;
+        if (x < 2) return x;
+        long long low = 1, high = x;
+        int ans = 0;
+        while (low <= high) {
+            long long mid = low + (high - low) / 2;
+
+            if (mid * mid == x) {
+                return mid;
             }
-            else if(pro>x && prev<x){
-                return i-1;
+            else if (mid * mid < x) {
+                ans = mid;
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
             }
         }
-        return 0;
+
+        return ans;
     }
 };
