c8bbb5901e986c1965bafbef5115d56677a0879c
Pavan Ashok Sambare Patil <pavansambare084@gmail.com>
2026-08-13 21:36:42 +0530

    Time: 0 ms (100%), Space: 17.7 MB (66.71%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     string largestOddNumber(string num) {
-        string ans;
         int n = num.length();
         for(int i=n-1;i>=0;i--){
             int k= num[i] - '0';
             if(k%2!=0){
-                for(int j=0;j<=i;j++){
-                    ans+=num[j];
-                }
-                break;
+                return num.substr(0,i+1);
             }
         }
-        return ans;
+        return "";
     }
 };
