76d88c6a3303d2c6f4894279c18ad86e7b543825
Pavan Ashok Sambare Patil <pavansambare084@gmail.com>
2026-08-16 22:11:18 +0530

    Time: 79 ms (25.05%), Space: 113.3 MB (16.28%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int ,int> mpp;
+        for(int i=0;i<nums.size();i++){
+            mpp[nums[i]]++;
+        }
+        for(auto x:mpp){
+            if(x.second>=2) return 1;
+        }
+        return 0;
+    }
+};
