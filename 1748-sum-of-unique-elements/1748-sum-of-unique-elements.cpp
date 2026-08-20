47e5e415efcd6cc6a1d9681a29144d842af04cc4
Pavan Ashok Sambare Patil <pavansambare084@gmail.com>
2026-08-20 14:17:08 +0530

    Time: 0 ms (100%), Space: 10.8 MB (18.79%) - LeetHub

---
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        unordered_map<int,int>mpp;
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            mpp[nums[i]]++;
+        }
+        for(auto it:mpp){
+            if(it.second==1) sum+=it.first;
+        }
+        return sum;
+    }
+};
