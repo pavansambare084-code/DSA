d7f934fa3c69382860ab918bb8022b122bbb652d
Pavan Ashok Sambare Patil <pavansambare084@gmail.com>
2026-08-23 22:31:35 +0530

    Time: 1035 ms (53.35%), Space: 7.6 MB (88.73%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        if (num==1) return 0;
         int sum=0;
         for(int i=1;i<=(num/2);i++){
             if(num%i==0) sum+=i;
         }
         if(sum==1) return 0;
-        if(num%sum==0) return 1;
+        if(num==sum) return 1;
         return 0;
     }
 };
